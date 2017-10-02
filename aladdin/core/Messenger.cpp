64a34aabeb9088fddb661985fe761241a8d4dce9
Lê Phương Khanh <khanhlp315@gmail.com>
2017-10-02 10:03:02 +0700

    Adds memory debugging for messenger

--- aladdin/core/Messenger.cpp
+++ aladdin/core/Messenger.cpp
@@ -4,6 +4,16 @@ NAMESPACE_ALA
 {
 ALA_CLASS_SOURCE_0(ala::Messenger)
 
+Messenger::Messenger ( )
+{
+  TOTAL_MESSENGERS_CREATED++;
+}
+
+Messenger::~Messenger ( )
+{
+  TOTAL_MESSENGERS_DELETED++;
+}
+
 void Messenger::broadcast (std::string messageId, IMessageArg* args )
 {
 	for(auto callback: _callbacks[messageId])
@@ -23,4 +33,7 @@ void Messenger::unsubscribe (std::string messageId,long token )
 {
   _callbacks[messageId].erase(token);
 }
+
+long Messenger::TOTAL_MESSENGERS_CREATED = 0;
+long Messenger::TOTAL_MESSENGERS_DELETED = 0;
 }
--- aladdin/core/Messenger.h
+++ aladdin/core/Messenger.h
@@ -7,12 +7,17 @@ NAMESPACE_ALA
 {
 ALA_CLASS_HEADER_0(Messenger)
 public:
+  Messenger();
+  ~Messenger();
   void broadcast(std::string messageId, IMessageArg* args);
   long subscribe(std::string messageId, std::function<void(IMessageArg*)> f);
   void unsubscribe(std::string messageId, long token);
 private:
   std::map<std::string, std::map<long, std::function<void(IMessageArg*)>>> _callbacks;
   long _token = 0;
+public:
+  static long TOTAL_MESSENGERS_CREATED;
+  static long TOTAL_MESSENGERS_DELETED;
 };
 }
 #endif //__ALADDIN_CORE_MESSENGER__
