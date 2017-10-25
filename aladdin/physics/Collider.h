1f3eeda74724746860f308c3e4355621aeaafbc4
Lê Phương Khanh <khanhlp315@gmail.com>
2017-10-25 15:10:13 +0700

    Create collider & rigidbody

---
+++ aladdin/physics/Collider.h
@@ -0,0 +1,10 @@
+﻿#ifndef __ALADDIN_PHYSICS_COLLIDER_H__
+#define __ALADDIN_PHYSICS_COLLIDER_H__
+
+class Collider
+{
+public:
+  
+};
+
+#endif //!__ALADDIN_PHYSICS_COLLIDER_H__
---
+++ aladdin/physics/Rigidbody.h
@@ -0,0 +1,18 @@
+﻿#ifndef __ALADDIN_PHYSICS_RIGIDBODY_H__
+#define __ALADDIN_PHYSICS_RIGIDBODY_H__
+
+#include "../core/GameObjectComponent.h"
+#include "../core/Vec2.h"
+
+NAMESPACE_ALA
+{
+ALA_CLASS_HEADER_1(Rigidbody, ala::GameObjectComponent)
+private:
+  Vec2 _position;
+  Vec2 _velocity;
+  Vec2 _accelerator;
+public:
+
+};
+}
+#endif //!__ALADDIN_PHYSICS_RIGIDBODY_H__
