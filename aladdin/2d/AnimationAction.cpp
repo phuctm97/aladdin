d2e521f103febd2979eda5d2d4dd62a1235ee4c7
Minh-Phuc Tran <phuctm97@gmail.com>
2017-11-14 16:41:11 +0700

    Remove anchor point

--- aladdin/2d/AnimationAction.cpp
+++ aladdin/2d/AnimationAction.cpp
@@ -6,10 +6,9 @@ ALA_CLASS_SOURCE_0(ala::AnimationAction)
 
 AnimationAction::AnimationAction ( ): _loop( false ), _interval( 0 ) {}
 
-AnimationAction::AnimationAction(const std::vector<ala::Rect>& frames, const std::vector<Vec2>& frameAnchors, const std::string& actionName, const std::string& textureName, const float interval, const bool loop)
+AnimationAction::AnimationAction(const std::vector<ala::Rect>& frames, const std::string& actionName, const std::string& textureName, const float interval, const bool loop)
 {
   _frames = frames;
-  _frameAnchors = frameAnchors;
   _interval = interval;
   _loop = loop;
   _actionName = actionName;
@@ -23,11 +22,7 @@ const std::vector<ala::Rect>& AnimationAction::getFrames() const
   return _frames;
 }
 
-const std::vector<Vec2>& AnimationAction::getFrameAnchors() const {
-  return _frameAnchors;
-}
-
-  float AnimationAction::getInterval() const
+float AnimationAction::getInterval() const
 {
   return _interval;
 }
