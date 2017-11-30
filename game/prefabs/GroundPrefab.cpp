99d7e10e433e7b36e9d1a94d156dd71fa287150c
Minh-Phuc Tran <phuctm97@gmail.com>
2017-11-30 21:50:17 +0700

    Add sword collider to aladdin prefab

--- game/prefabs/GroundPrefab.cpp
+++ game/prefabs/GroundPrefab.cpp
@@ -10,11 +10,11 @@ void GroundPrefab::doInstantiate( ala::GameObject* object ) const {
 
   // components
   const auto body = new Rigidbody( object, PhysicsMaterial(), ALA_BODY_TYPE_STATIC );
-  const auto collider1 = new Collider( object, false, Vec2( 0, 0 ), groundSize, 0, "firstGround" );
+  const auto collider1 = new Collider( object, false, Vec2( 0, 0 ), groundSize, 1, 0, "firstGround" );
   const auto colliderRenderer1 = new ColliderRenderer( collider1 );
 
   const auto collider2 = new Collider( object, false, Vec2( groundSize.getWidth(), -20 ),
-                                       groundSize, 0, "secondGround" );
+                                       groundSize, 1, 0, "secondGround" );
   const auto colliderRenderer2 = new ColliderRenderer( collider2 );
 
   const auto transform = object->getTransform();
