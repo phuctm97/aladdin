bab4ff1db528dc854dff6cf3cb3c23aef705b3ec
KHUONG-PC\4pro <khuongnb1997@gmail.com>
2017-12-18 07:25:09 +0700

    Positioning completed <Note: +Pendal, fix the enemy standing on switching stair>

--- game/scenes/AgrabahMarketScene.cpp
+++ game/scenes/AgrabahMarketScene.cpp
@@ -17,9 +17,6 @@ void AgrabahMarketScene::onPreInitialize() {
 	// configurations
 	enablePhysics(Vec2(0, -500.0f));
 
-	
-
-	//new CameraController(getMainCamera());
 
 	// initial objects
 	gameManager->getPrefabV2("Sprite")->instantiateWithArgs("background.png 0", "Background")
@@ -53,7 +50,6 @@ void AgrabahMarketScene::onPreInitialize() {
 
 	gameManager->getPrefabV2("Agrabah Market Stairs 6")->instantiateWithArgs("0", "Stairs 5")
 		->getTransform()->setPosition(4214.5f, -76.5f);*/
-
 
 	/* Platforms */
 	/*gameManager->getPrefabV2("Platform")->instantiateWithArgs("295")
@@ -145,7 +141,6 @@ void AgrabahMarketScene::onPreInitialize() {
 		->getTransform()->setPosition(Vec2(4165.5f, 428.0f));*/
 
 	/* Ropes */
-
 	/*gameManager->getPrefabV2( "Climbable Rope" )->instantiateWithArgs( "280" )
 			   ->getTransform()->setPosition( Vec2( 1931, 252 ) );
 
@@ -158,7 +153,6 @@ void AgrabahMarketScene::onPreInitialize() {
 	gameManager->getPrefabV2("Climbable Rope")->instantiateWithArgs("200")
 		->getTransform()->setPosition(Vec2(4555.0f, 220.5f));*/
 	
-	
 	/* Bars */
 	/*gameManager->getPrefabV2( "Holdable Bar" )->instantiateWithArgs( "412" )
 		->getTransform()
@@ -168,24 +162,32 @@ void AgrabahMarketScene::onPreInitialize() {
 		->getTransform()
 		->setPosition(4391.0f, 487.0f);*/
 
-	/* Springs
+	/* Springs*/
+	/*gameManager->getPrefabV2( "Jumpable Spring" )->instantiateWithArgs( "" )
+		->getTransform()->setPosition(Vec2(3302.5f, 323.0f));
 
 	gameManager->getPrefabV2( "Jumpable Spring" )->instantiateWithArgs( "" )
-			   ->getTransform()
-			   ->setPosition( 0, 42 );
-
-	*/
-
-	/* Checkpoints
-	gameManager->getPrefabV2( "Checkpoint" )->instantiateWithArgs( "" )
+		->getTransform()->setPosition(Vec2(3319.5f, 403.0f));
+	
+	gameManager->getPrefabV2("Jumpable Spring")->instantiateWithArgs("")
+		->getTransform()->setPosition(Vec2(4084.0f, 452.0f));
+*/
+	
+	/* Checkpoints */
+	/*gameManager->getPrefabV2( "Checkpoint" )->instantiateWithArgs( "" )
 			->getTransform()
 			->setPosition( Vec2( 2545, 73 ) );
 
-	*/
+	gameManager->getPrefabV2("Checkpoint")->instantiateWithArgs("")
+		->getTransform()
+		->setPosition(Vec2(3519.5f, 379.0f));
 
-	/* Charcoal Burners
-
-	gameManager->getPrefabV2( "Charcoal Burner" )->instantiateWithArgs( "90", "Charcoal Burner 1" )
+	gameManager->getPrefabV2("Checkpoint")->instantiateWithArgs("")
+		->getTransform()
+		->setPosition(Vec2(3346.0f, 108.5f));*/
+	
+	/* Charcoal Burners */
+	/*gameManager->getPrefabV2( "Charcoal Burner" )->instantiateWithArgs( "90", "Charcoal Burner 1" )
 			->getTransform()
 			->setPosition( Vec2( 360.5f, -50 ) );
 
@@ -203,112 +205,217 @@ void AgrabahMarketScene::onPreInitialize() {
 
 	gameManager->getPrefabV2( "Charcoal Burner" )->instantiateWithArgs( "110", "Charcoal Burner 4" )
 			->getTransform()
-			->setPosition( Vec2( 2987, -82 ) );
+			->setPosition( Vec2( 2987, -82 ) );*/
 
-	*/
-
-	/* Camels
-
-	gameManager->getPrefabV2( "Camel" )->instantiateWithArgs( "R" )
+	/* Camels */
+	/*gameManager->getPrefabV2( "Camel" )->instantiateWithArgs( "R" )
 			->getTransform()
-			->setPosition( Vec2( 1404.5, -57.5 ) );
+			->setPosition( Vec2( 1404.5, -57.5 ) );*/
 
-	*/
-
-	/* Hand Enemies
-
-	gameManager->getPrefabV2( "Hand Enemy" )->instantiateWithArgs( "R 45 80 0 -80", "Hand Enemy 1" )
+	/* Hand Enemies ? ? :D ? ?*/
+	/*gameManager->getPrefabV2( "Hand Enemy" )->instantiateWithArgs( "R 45 80 0 -80", "Hand Enemy 1" )
 			->getTransform()
 			->setPosition( Vec2( 1680.0f, 57.5f ) );
 
 	gameManager->getPrefabV2( "Hand Enemy" )->instantiateWithArgs( "R 45 100 0 -102" )
 			->getTransform()
 			->setPosition( Vec2( 1632.5f, 89.5f ) );
-
-
-
-	/* Eatable Apples
-
-	gameManager->getPrefabV2( "Eatable Apple" )->instantiateWithArgs( "", "Apple 1" )
-			->getTransform()
-			->setPosition( Vec2( 0, 10 ) );
-
 	*/
 
-	/* Eatable Hearts
-
-	gameManager->getPrefabV2( "Eatable Heart" )->instantiateWithArgs( "", "Heart 1" )
+	/* Eatable Apples */
+	//float appleX[] = { //325.5f, 355.0f, 388.0f, 691.5f, 723.0f, 755.5f, 869.5f, 901.5f, 932.0f,1363.0f, 1396.5f, 1409.0f, 1490.5f, 1526.0f, 1559.0f, 1589.0f, 1619.5f, 1839, 1845, 1869, 1978, 2006, 2011,
+	//	//1607, 1402, 1338, 1338, 1346, 1361, 1616, 1654, 1690, 1824, 1858, 1900, 2132, 2164, 2190, 2190, 2190, 2190, 2190
+	//	//2100, 2115, 2131, 2257, 2373, 2272,2727, 2727, 2727, 2947, 2983, 3018, 3181, 3244, 3211, 3348, 3414, 3381, 3682, 3744, 3713, 3936, 3999, 3968, 4489, 4396, 4347, 4290,
+	//	4050, 3944, 3782, 3746, 3700, 3370, 3334, 3298, 3254, 3212, 3170, 2776, 2738, 2700, 2488, 2704, 2720, 2740, 3146, 3162, 3194, 3458, 4136, 4164, 4164, 4192 };
+	//float appleY[] = { //27.5f, 43.0f, 27.5f, 25.5f, 44.5f, 25.5f, 27.0f, 45.0f, 27.0f, 90.5f, 75.0f, 41.0f, 14.5f, 29.5f, 14.5f, 29.5f, 14.5f, 24, 60, 95, 95, 56, 24,
+	//	//206, 181, 255, 296, 333, 362,380, 392, 380, 378, 402, 378, 378, 376, 340, 284, 232, 184, 142
+	//	//51, 70, 51, -4, -26, 85,82, 35, -15, -9, 9, -10, -10, -10, 8, -9, -9, 9, -9, -8, 10, -5, -5, 9, 165, 221, 240, 221
+	//	155, 137, 201, 191, 157, 167, 185, 167, 167, 193, 167, 229, 247, 231, 365, 337, 359, 337, 459, 491, 505, 395, 543, 563, 525, 541 };
+	
+	////for (size_t i = 0; i<sizeof(appleX); i++)
+	//for (int i = 0; i<28; i++)
+	//{
+	//	gameManager->getPrefabV2( "Eatable Apple" )->instantiateWithArgs( "", "Apple 1" )
+	//		->getTransform()->setPosition( Vec2( appleX[i], appleY[i] ) );
+	//}
+	
+	/* Eatable Hearts */
+	/*gameManager->getPrefabV2( "Eatable Heart" )->instantiateWithArgs( "", "Heart 1" )
 			->getTransform()
-			->setPosition( Vec2( 0, 10 ) );
+			->setPosition( Vec2(2232.0f, -68.5f) );
+	
+	gameManager->getPrefabV2("Eatable Heart")->instantiateWithArgs("", "Heart 1")
+		->getTransform()
+		->setPosition(Vec2(3077.0f, 186.5f));
+	
+	gameManager->getPrefabV2("Eatable Heart")->instantiateWithArgs("", "Heart 1")
+		->getTransform()
+		->setPosition(Vec2(2875.5f, 504.0f));*/
 
-	*/
-
-	/* Genie Lamps
+	/* Genie Lamps */
+	/*gameManager->getPrefabV2("Genie Lamp")->instantiateWithArgs("", "Genie Lamp 1")
+			->getTransform()
+			->setPosition(Vec2(1849.5f, 142.0f));
 
 	gameManager->getPrefabV2("Genie Lamp")->instantiateWithArgs("", "Genie Lamp 1")
-			->getTransform()
-			->setPosition(Vec2(600, 10));
-
-	*/
-
-	/* Genie Bonuses
-
-	gameManager->getPrefabV2( "Genie Bonus" )->instantiateWithArgs( "", "Genie Bonus 1" )
-			->getTransform()
-			->setPosition( Vec2( 0, 10 ) );
-
-	*/
-
-	/* Spend Theses
-
-	gameManager->getPrefabV2( "Spend These" )->instantiateWithArgs( "", "Spend These 1" )
 		->getTransform()
-		->setPosition( Vec2( 0, 10 ) );
+		->setPosition(Vec2(2309.5f, 47.0f));
+	
+	gameManager->getPrefabV2("Genie Lamp")->instantiateWithArgs("", "Genie Lamp 1")
+		->getTransform()
+		->setPosition(Vec2(2899.5f, 159.5f));*/
+	
+	/* Genie Bonuses */
+	/*gameManager->getPrefabV2( "Genie Bonus" )->instantiateWithArgs( "", "Genie Bonus 1" )
+			->getTransform()
+			->setPosition( Vec2(1474.5f, 172.0f));
+	
+	gameManager->getPrefabV2("Genie Bonus")->instantiateWithArgs("", "Genie Bonus 1")
+		->getTransform()
+		->setPosition(Vec2(2094.5f, -41.0f));
+	
+	gameManager->getPrefabV2("Genie Bonus")->instantiateWithArgs("", "Genie Bonus 1")
+		->getTransform()
+		->setPosition(Vec2(3992.0f, 184.0f));
 
-	*/
+	gameManager->getPrefabV2("Genie Bonus")->instantiateWithArgs("", "Genie Bonus 1")
+		->getTransform()
+		->setPosition(Vec2(2375.5f, 494.0f));
+	
+	gameManager->getPrefabV2("Genie Bonus")->instantiateWithArgs("", "Genie Bonus 1")
+		->getTransform()
+		->setPosition(Vec2(2961.5f, 359.0f));*/
 
-	/* Abu Bonus
+	/* Spend Theses */
+	/*gameManager->getPrefabV2( "Spend These" )->instantiateWithArgs( "", "Spend These 1" )
+		->getTransform()
+		->setPosition(Vec2(2182.0f, -71.0f));
 
-	gameManager->getPrefabV2( "Abu Bonus" )->instantiateWithArgs( "", "Abu Bonus 1" )
+	gameManager->getPrefabV2("Spend These")->instantiateWithArgs("", "Spend These 1")
+		->getTransform()
+		->setPosition(Vec2(2497.0f, -53.5f));
+
+	gameManager->getPrefabV2("Spend These")->instantiateWithArgs("", "Spend These 1")
+		->getTransform()
+		->setPosition(Vec2(2552.0f, -53.5f));
+
+	gameManager->getPrefabV2("Spend These")->instantiateWithArgs("", "Spend These 1")
+		->getTransform()
+		->setPosition(Vec2(2488.5f, 329.0f));
+
+	gameManager->getPrefabV2("Spend These")->instantiateWithArgs("", "Spend These 1")
+		->getTransform()
+		->setPosition(Vec2(2488.5f, 300.0f));
+	
+	gameManager->getPrefabV2("Spend These")->instantiateWithArgs("", "Spend These 1")
+		->getTransform()
+		->setPosition(Vec2(2849.5f, 480.0f));
+
+	gameManager->getPrefabV2("Spend These")->instantiateWithArgs("", "Spend These 1")
+		->getTransform()
+		->setPosition(Vec2(2879.5f, 461.0f));
+
+	gameManager->getPrefabV2("Spend These")->instantiateWithArgs("", "Spend These 1")
+		->getTransform()
+		->setPosition(Vec2(2908.5f, 480.0f));*/
+		
+	/* Abu Bonus */
+	/*gameManager->getPrefabV2( "Abu Bonus" )->instantiateWithArgs( "", "Abu Bonus 1" )
 		 ->getTransform()
-		 ->setPosition( Vec2( 0, 10 ) );
+		 ->setPosition(Vec2(3143.0f, 404.0f));*/
 
-	*/
-
-	/* Peddlers
-
-	gameManager->getPrefabV2( "Peddler" )->instantiateWithArgs( "", "Peddler 1" )
+	/* Peddlers */
+	/*gameManager->getPrefabV2( "Peddler" )->instantiateWithArgs( "", "Peddler 1" )
 		->getTransform()
-		->setPosition( Vec2( 0, 10 ) );
+		->setPosition(Vec2(3790.5f, 386.5f));*/
 
-	*/
-
-	/* Guards
-
-	gameManager->getPrefabV2( "Guard 1" )->instantiateWithArgs( "550 430 670", "Guard 1" )
+	/* Guards */
+	/*gameManager->getPrefabV2( "Guard 1" )->instantiateWithArgs( "550 430 670", "Guard 1" )
 	   ->getTransform()->setPositionY( -25 );
 
 	gameManager->getPrefabV2( "Guard 2" )->instantiateWithArgs( "818 595 1010", "Guard 2" )
-	   ->getTransform()->setPositionY( -25 );
+	   ->getTransform()->setPositionY( -25 ); 
 
 	gameManager->getPrefabV2( "Guard 3" )->instantiateWithArgs( "1559 1380 1699", "Guard 3" )
-	   ->getTransform()->setPositionY( -25 );
+	   ->getTransform()->setPositionY( -25 ); 
 
 	gameManager->getPrefabV2( "Guard 4" )->instantiateWithArgs( "1169", "Guard 4" )
 	   ->getTransform()->setPositionY( 10 );
 
-	gameManager->getPrefabV2( "Guard 5" )->instantiateWithArgs( "550 430 670", "Guard 5" )
-	   ->getTransform()->setPositionY( -25 );
+	gameManager->getPrefabV2("Guard 3")->instantiateWithArgs("1660 1597 1875", "Guard 3")
+		->getTransform()->setPositionY(163);
 
-	*/
+	gameManager->getPrefabV2("Guard 2")->instantiateWithArgs("1567 1437 2102", "Guard 2")
+		->getTransform()->setPositionY(336);
 
-	//gameManager->getPrefabV2("Playable Aladdin")->instantiateWithArgs("-80 -25", "Aladdin");
-	gameManager->getPrefabV2("Playable Aladdin")->instantiateWithArgs("3872 200", "Aladdin");
+	gameManager->getPrefabV2("Guard 3")->instantiateWithArgs("2050 1437 2102", "Guard 3")
+		->getTransform()->setPositionY(336);
+
+	gameManager->getPrefabV2("Guard 4")->instantiateWithArgs("2370", "Guard 4")
+		->getTransform()->setPositionY(81);
+
+	gameManager->getPrefabV2("Guard 3")->instantiateWithArgs("2657 2577 2680", "Guard 3")
+		->getTransform()->setPositionY(43);
+
+	gameManager->getPrefabV2("Guard 2")->instantiateWithArgs("3080 2662 4170", "Guard 2")
+		->getTransform()->setPositionY(-40);
+
+	gameManager->getPrefabV2("Guard 3")->instantiateWithArgs("3305 2662 4170", "Guard 3")
+		->getTransform()->setPositionY(-40);
+
+	gameManager->getPrefabV2("Guard 2")->instantiateWithArgs("3507 2662 4170", "Guard 2")
+		->getTransform()->setPositionY(-40);
+
+	gameManager->getPrefabV2("Guard 2")->instantiateWithArgs("4137 2662 4170", "Guard 2")
+		->getTransform()->setPositionY(-40);
+
+	gameManager->getPrefabV2("Guard 3")->instantiateWithArgs("4498 4315 4607", "Guard 3")
+		->getTransform()->setPositionY(-6);
+
+	gameManager->getPrefabV2("Guard 4")->instantiateWithArgs("4190", "Guard 4")
+		->getTransform()->setPositionY(150);
+
+	gameManager->getPrefabV2("Guard 3")->instantiateWithArgs("3740 3685 3800", "Guard 3")
+		->getTransform()->setPositionY(160);
+
+	gameManager->getPrefabV2("Guard 2")->instantiateWithArgs("3630 2902 3660", "Guard 2")
+		->getTransform()->setPositionY(123);
+
+	gameManager->getPrefabV2("Guard 3")->instantiateWithArgs("2992 3685 3800", "Guard 3")
+		->getTransform()->setPositionY(160);
+
+	gameManager->getPrefabV2("Guard 3")->instantiateWithArgs("2832 2660 2922", "Guard 3")
+		->getTransform()->setPositionY(191);
+
+	gameManager->getPrefabV2("Guard 5")->instantiateWithArgs("2697 2660 2922", "Guard 5")
+		->getTransform()->setPositionY(191);
+
+	gameManager->getPrefabV2("Guard 4")->instantiateWithArgs("2378", "Guard 4")
+		->getTransform()->setPositionY(443);
+
+	gameManager->getPrefabV2("Guard 4")->instantiateWithArgs("2952", "Guard 4")
+		->getTransform()->setPositionY(443);
+
+	gameManager->getPrefabV2("Guard 2")->instantiateWithArgs("2915 2662 3030", "Guard 2")
+		->getTransform()->setPositionY(319);
+
+	gameManager->getPrefabV2("Guard 3")->instantiateWithArgs("3135 3037 3385", "Guard 3")
+		->getTransform()->setPositionY(286);
+
+	gameManager->getPrefabV2("Guard 2")->instantiateWithArgs("4357 4192 4535", "Guard 2")
+		->getTransform()->setPositionY(400);*/
+
+	///////// ALADDIN //////////////
+	new CameraController(getMainCamera());
+	gameManager->getPrefabV2("Playable Aladdin")->instantiateWithArgs("-80 -25", "Aladdin");
+	
+	/* Dev support*/
+	/*gameManager->getPrefabV2("Playable Aladdin")->instantiateWithArgs("3872 200", "Aladdin");
 
 	const auto devNode = gameManager->getPrefab("Node")->instantiate();
 	devNode->setLayer("Debug");
-	devNode->getTransform()->setPosition(Vec2(4555.0f, 220.5f));
+	devNode->getTransform()->setPosition(Vec2(600, 0));
 
-	new DebugCameraController(getMainCamera(), devNode);
+	new DebugCameraController(getMainCamera(), devNode);*/
 
 }
--- game/scripts/NodeController.cpp
+++ game/scripts/NodeController.cpp
@@ -3,7 +3,9 @@
 USING_NAMESPACE_ALA;
 
 NodeController::NodeController(ala::GameObject* gameObject, const std::string& name)
-	: GameObjectComponent(gameObject, name), _logger("NodeController"), _speed(1) { }
+	: GameObjectComponent(gameObject, name), _logger("NodeController"), _speed(1) {
+	_appleCount = -1;
+}
 
 void NodeController::onUpdate(const float delta) {
 	const auto input = Input::get();
@@ -70,24 +72,22 @@ void NodeController::onUpdate(const float delta) {
 		}
 	}
 
-	if (input->getKeyDown(ALA_KEY_F3)) {
+	if (input->getKeyDown(ALA_KEY_F2)) {
 		_speed += 0.5f;
 		_logger.info("Moving speed = %f\n", _speed);
 	}
 
-	if (input->getKeyDown(ALA_KEY_F2)) {
+	if (input->getKeyDown(ALA_KEY_F1)) {
 		_speed -= 0.5f;
 		_logger.info("Moving speed = %f\n", _speed);
 	}
 
-	if (input->getKeyDown(ALA_KEY_F1)) {
-		_logger.info("X: %f; Y: %f \n", transform->getPositionX(), transform->getPositionY());
+	if (input->getKeyDown(ALA_KEY_F3)) {
 		if (rectRenderer != NULL) {
-			_logger.info("Rect Size (%f , %f) \n", rectRenderer->getSize().getWidth(), rectRenderer->getSize().getHeight());
-		}
-
-		if (collider != NULL) {
-			_logger.info("Collider Size (%f , %f) \n", collider->getSize().getWidth(), collider->getSize().getHeight());
+			//if (_rectObject != NULL) _rectObject->release();
+			//_rectObject = new GameObject(GameManager::get()->getRunningScene());
+			new RectRenderer(new GameObject(GameManager::get()->getRunningScene(), "S"+std::to_string(_appleCount)) , rectRenderer->getOffset(), rectRenderer->getSize(), Color(0, 255, 255), "");
+			GameManager::get()->getObjectByName("S"+ std::to_string(_appleCount))->getTransform()->setPosition(transform->getPosition());
 		}
 	}
 
@@ -102,40 +102,27 @@ void NodeController::onUpdate(const float delta) {
 	}
 
 	if (input->getKeyDown(ALA_KEY_F5)) {
-		if (collider != NULL) {
-			_logger.info(
-				//StairNoob//"\n->getTransform()->setPosition(Vec2(%.0f,%.0f));\n((Collider*)->getComponent(\"first\"))->setSize(Size(%.0f, %.0f));\n((Collider*)->getComponent(\"first\"))->setOffset(Vec2(%.0f, %.0f));\n((Collider*)->getComponent(\"second\"))->setSize(Size(%.0f, %.0f));\n((Collider*)->getComponent(\"second\"))->setOffset(Vec2(%.0f, %.0f)); ",
-				"\n->getTransform()->setPosition(Vec2(%.1ff, %.1ff));\n->getComponentT<Collider>()->setSize(Size(%.1ff, %.1ff));",
-				transform->getPositionX(),
-				transform->getPositionY(),
-				collider->getSize().getWidth(),
-				collider->getSize().getHeight());
+		if (rectRenderer != NULL) {
+			_appleCount++;
+			_appleX[_appleCount] = transform->getPositionX();
+			_appleY[_appleCount] = transform->getPositionY();
+			new RectRenderer(new GameObject(GameManager::get()->getRunningScene(), "S" + std::to_string(_appleCount)), rectRenderer->getOffset(), rectRenderer->getSize(), Color(0, 255, 255), "");
+			GameManager::get()->getObjectByName("S" + std::to_string(_appleCount))->getTransform()->setPosition(transform->getPosition());
 		}
 	}
 
 	if (input->getKeyDown(ALA_KEY_F6)) {
-			if (collider != NULL) {
-				if (_rectObject != NULL) _rectObject->release();
-				_rectObject = new GameObject(GameManager::get()->getRunningScene());
-				new RectRenderer(_rectObject, collider->getOffset(), collider->getSize(), Color(0, 255, 255), "");
-				_rectObject->getTransform()->setPosition(transform->getPosition());
-				_logger.info(
-					//StairNoob//"\n->getTransform()->setPosition(Vec2(%.0f,%.0f));\n((Collider*)->getComponent(\"first\"))->setSize(Size(%.0f, %.0f));\n((Collider*)->getComponent(\"first\"))->setOffset(Vec2(%.0f, %.0f));\n((Collider*)->getComponent(\"second\"))->setSize(Size(%.0f, %.0f));\n((Collider*)->getComponent(\"second\"))->setOffset(Vec2(%.0f, %.0f)); ",
-					//Stair??//"\n->getTransform()->setPosition(Vec2(%.1ff, %.1ff));\n->getComponentT<Collider>()->setSize(Size(%.1ff, %.1ff));",
-					//Stair Group 2 //"\nconst auto = gameManager->getPrefab(\"StairGroupTwo\")->instantiate();\n->getTransform()->setPosition(Vec2(%.1ff, %.1ff));\n->getComponentT<Collider>()->setSize(Size(%.1ff, %.1ff)); ",
-					//Somthing//"\nconst auto = gameManager->getPrefab(\"\")->instantiate();\n->getTransform()->setPosition(Vec2(%.1ff, %.1ff));\n->getComponentT<Collider>()->setSize(Size(%.1ff, %.1ff)); ",
-					"\nconst auto = gameManager->getPrefab(\"Platform\")->instantiate();\n->getTransform()->setPosition(Vec2(%.2ff, %.2ff));\nstatic_cast<Collider*>(->getComponent(\"Upon\"))->setSize(Size(%.2ff, %.2ff));\nstatic_cast<Collider*>(->getComponent(\"Below\"))->setSize(Size(%.2ff, %.2ff));\nstatic_cast<Collider*>(->getComponent(\"Upon\"))->setOffset(Vec2(0, %.2ff));\nstatic_cast<Collider*>(->getComponent(\"Below\"))->setOffset(Vec2(0, -%.2ff));",
-					transform->getPositionX(),
-					transform->getPositionY(),
-					collider->getSize().getWidth(),
-					collider->getSize().getHeight() / 2,
-					collider->getSize().getWidth(),
-					collider->getSize().getHeight() / 2,
-					collider->getSize().getHeight() / 4,
-					collider->getSize().getHeight() / 4 + 0.75f	);
-			}
-		
+		string s = "";
+		for (int i = 0; i <= _appleCount; i++) {
+			s += std::to_string((int)_appleX[i]) + ", ";
+		}
+		_logger.info("%s", s.c_str());
 
+		s = "";
+		for (int i = 0; i <= _appleCount; i++) {
+			s += std::to_string((int)_appleY[i]) + ", ";
+		}
+		_logger.info("%s", s.c_str());
 	}
 }
 
