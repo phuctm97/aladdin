15b73fe4f28813545f588b8623c21fb64be67ee7
Minh-Phuc Tran <phuctm97@gmail.com>
2017-12-21 12:12:50 +0700

    Fix merge

--- game/scenes/AutoLoadScene.cpp
+++ game/scenes/AutoLoadScene.cpp
@@ -11,12 +11,10 @@ void AutoLoadScene::onPreInitialize() {
   const auto gameManager = GameManager::get();
 
   // configurations
-  /*if ( _useCameraController ) {
+  if ( _useCameraController ) {
     new CameraController( getMainCamera() );
-  }*/
+  }
 
   // scene loader
   gameManager->loadScene( _sceneName );
-
-  new DebugCameraController(getMainCamera(), gameManager->getObjectByName("Node"));
 }
